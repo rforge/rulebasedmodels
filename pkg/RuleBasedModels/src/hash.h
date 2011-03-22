916f1895ebb3e77eb9a347651b4cad779b75144d
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-22 20:44:29 +0000

    Fixed name of two arguments.  Added more comments.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rulebasedmodels@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RuleBasedModels/src/hash.h
+++ pkg/RuleBasedModels/src/hash.h
@@ -1,21 +1,33 @@
 #ifndef _HASH_H_
 #define _HASH_H_
 
-/* Core functions */
+/* Used internally and for testing */
 extern unsigned int hashCode(const char *key);
+
+/* Constructor and destructor */
 extern void *ht_new(int size);
 extern void ht_destroy();
+
+/* Hash table iteration functions */
 extern void *ht_next(void *ht);
 extern void ht_reset(void *ht);
-extern int ht_delete(void *ht, const char *key);
-extern void *ht_lookup(void *ht, const char *key);
-extern char *ht_key(void *ht);
-extern void *ht_value(void *ht);
 
-/* Convenience functions */
+/* Hash entry delete function */
+extern int ht_delete(void *ht, const char *key);
+
+/* Low level hash table accessor function */
+extern void *ht_lookup(void *ht, const char *key);
+
+/* Hash entry accessor functions */
+extern char *ht_key(void *entry);
+extern void *ht_value(void *entry);
+
+/* Hash table setting functions */
 extern int ht_setvoid(void *ht, const char *key, void *value);
 extern int ht_setint(void *ht, const char *key, int value);
 extern int ht_setstr(void *ht, const char *key, char *value);
+
+/* Hash table accessor functions */
 extern void *ht_getvoid(void *ht, const char *key, void *defval, void *errval);
 extern int ht_getint(void *ht, const char *key, int defval, int errval);
 extern char *ht_getstr(void *ht, const char *key, char *defval, char *errval);
