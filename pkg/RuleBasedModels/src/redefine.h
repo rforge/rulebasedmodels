2e183ee29c3728bc6e1cbccad0c15e56b935fba7
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-22 01:41:52 +0000

    Added rbm_init, and added extern to function declarations.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rulebasedmodels@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RuleBasedModels/src/redefine.h
+++ pkg/RuleBasedModels/src/redefine.h
@@ -9,23 +9,25 @@
 #define JMP_OFFSET 100
 extern jmp_buf rbm_buf;
 
-int rbm_register(STRBUF *sb, const char *filename, int force);
-int rbm_deregister(const char *filename);
-STRBUF *rbm_lookup(const char *filename);
-FILE *rbm_fopen(const char *filename, const char *mode);
-int rbm_fclose(FILE *stream);
-int rbm_fflush(FILE *stream);
-void rbm_rewind(FILE *stream);
-int rbm_fgetc(FILE *stream);
-int rbm_getc(FILE *stream);
-char *rbm_fgets(char *s, int n, FILE *stream);
-int rbm_fprintf(FILE *stream, const char *format, ...);
-int rbm_fputc(int c, FILE *stream);
-int rbm_putc(int c, FILE *stream);
-int rbm_fputs(const char *s, FILE *stream);
-size_t rbm_fwrite(const void *ptr, size_t size, size_t nitems, FILE *stream);
-int rbm_remove(const char *fname);
-void rbm_removeall();
-void rbm_exit(int status);
+extern int rbm_init();
+extern int rbm_register(STRBUF *sb, const char *filename, int force);
+extern int rbm_deregister(const char *filename);
+extern STRBUF *rbm_lookup(const char *filename);
+extern FILE *rbm_fopen(const char *filename, const char *mode);
+extern int rbm_fclose(FILE *stream);
+extern int rbm_fflush(FILE *stream);
+extern void rbm_rewind(FILE *stream);
+extern int rbm_fgetc(FILE *stream);
+extern int rbm_getc(FILE *stream);
+extern char *rbm_fgets(char *s, int n, FILE *stream);
+extern int rbm_fprintf(FILE *stream, const char *format, ...);
+extern int rbm_fputc(int c, FILE *stream);
+extern int rbm_putc(int c, FILE *stream);
+extern int rbm_fputs(const char *s, FILE *stream);
+extern size_t rbm_fwrite(const void *ptr, size_t size, size_t nitems,
+                         FILE *stream);
+extern int rbm_remove(const char *fname);
+extern void rbm_removeall();
+extern void rbm_exit(int status);
 
 #endif
