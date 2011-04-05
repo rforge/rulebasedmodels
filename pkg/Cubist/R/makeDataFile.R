29a5ae085a905f0758d6c7b0da03232243353b03
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-05 17:00:39 +0000

    added functionality for null y (test cases)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rulebasedmodels@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Cubist/R/makeDataFile.R
+++ pkg/Cubist/R/makeDataFile.R
@@ -2,7 +2,7 @@ makeDataFile <-
 function(x, y)
   {
     if(!is.data.frame(x)) x <- as.data.frame(x)
-    x <- cbind(y, x)
+    if(!is.null(y)) x <- cbind(y, x)
     out <- capture.output(
                           write.table(x,
                                       sep = ",",
