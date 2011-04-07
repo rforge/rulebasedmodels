15151c341cbe77fd85ce9bedfcb998304152b5be
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-07 18:57:42 +0000

    Put column of NA's into data frame if y is NULL.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rulebasedmodels@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Cubist/R/makeDataFile.R
+++ pkg/Cubist/R/makeDataFile.R
@@ -2,7 +2,8 @@ makeDataFile <-
 function(x, y)
   {
     if(!is.data.frame(x)) x <- as.data.frame(x)
-    if(!is.null(y)) x <- cbind(y, x)
+    if(is.null(y)) y <- rep(NA_real_, nrow(x))
+    x <- cbind(y, x)
     out <- capture.output(
                           write.table(x,
                                       sep = ",",
