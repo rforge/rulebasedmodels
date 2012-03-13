ecbe584c05095f3aa879259a3261efef99de3f36
pooryorick <pooryorick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-13 14:40:28 +0000

    add new stdioctcl, cfive, and tclcfive
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rulebasedmodels@198 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/C50/src/stdioctcl/include/stdioctcl.h
@@ -0,0 +1,92 @@
+/*
+ Copyright (C) 2011-2012, Nathan Coulter and others 
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "tcl.h"
+
+#ifndef _STDIOCTCL
+
+FILE * libc_stdin;
+FILE * libc_stdout;
+FILE * libc_stderr;
+
+FILE * stdioctcl_stdin;
+FILE * stdioctcl_stdout;
+FILE * stdioctcl_stderr;
+
+int stdioctcl_set_stdin (Tcl_Channel chan);
+int stdioctcl_set_stdout (Tcl_Channel chan);
+int stdioctcl_set_stderr (Tcl_Channel chan);
+
+typedef FILE *(*fopenPtr)(__const char *__restrict __filename,
+	__const char *__restrict __modes);
+fopenPtr libc_fopen;
+
+typedef int (*fclosePtr)(FILE*stream);
+fclosePtr libc_fclose;
+typedef int (*feofPtr)(FILE *stream);
+feofPtr libc_feof;
+typedef int (*fflushPtr)(FILE *stream);
+fflushPtr libc_fflush;
+typedef int (*fgetcPtr)(FILE * stream);
+fgetcPtr libc_fgetc;
+typedef char *(*fgetsPtr)
+	(char *__restrict __s, int __n, FILE *__restrict __stream);
+fgetsPtr libc_fgets;
+typedef int (*fprintfPtr)(FILE *stream ,const char *template ,...);
+fprintfPtr libc_fprintf;
+typedef int (*fputcPtr)(int c ,FILE *stream);
+fputcPtr libc_fputc;
+typedef int (*fputsPtr)(const char *S ,FILE *stream);
+fputsPtr libc_fputs;
+typedef size_t (*fwritePtr)
+	(const void *data ,size_t size ,size_t count ,FILE *stream);
+typedef size_t (*freadPtr)(void *data ,size_t size ,size_t count ,FILE *stream);
+freadPtr libc_fread;
+fwritePtr libc_fwrite;
+typedef int (*getcPtr)(FILE *stream);
+getcPtr libc_getc;
+typedef int (*putcharPtr)(int c);
+putcharPtr libc_putchar;
+typedef void (*rewindPtr)(FILE *stream);
+rewindPtr libc_rewind;
+typedef int (*snprintfPtr)(char *s ,size_t size ,const char * template ,...);
+snprintfPtr libc_snprintf;
+
+int stdioctcl_init ();
+Tcl_Interp *stdioctcl_interp_get();
+Tcl_Interp *stdioctcl_interp_set(Tcl_Interp *new);
+FILE * stdioctcl_fopen(const char *filename ,const char *opentype);
+int stdioctcl_fclose(FILE *stream);
+int stdioctcl_feof(FILE *stream);
+int stdioctcl_fflush(FILE *stream);
+int stdioctcl_fgetc(FILE *stream);
+#define stdioctcl_getc stdioctcl_fgetc 
+char * stdioctcl_fgets(char *s ,int count ,FILE *stream);
+int stdioctcl_fprintf(FILE *stream ,const char *template ,...);
+int stdioctcl_fprintf_ap(FILE *stream ,const char *template ,va_list ap);
+int stdioctcl_fputc(int c ,FILE *stream);
+int stdioctcl_fputs (const char *s ,FILE *stream);
+size_t stdioctcl_fread(void *data ,size_t size ,size_t count ,FILE *stream);
+size_t stdioctcl_fwrite(
+	const void *data ,size_t size ,size_t count ,FILE *stream);
+/* TODO:
+stdioctcl_freopen
+int stdioctcl_snprintf(char *s ,size_t size ,const char * template ,...);
+*/
+int stdioctcl_putchar(int c);
+void stdioctcl_rewind(FILE *stream);
+#define _STDIOCTCL
+#endif
+
