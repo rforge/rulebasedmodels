bc92a9d820482801e9ef495baa562a0075999d2c
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-19 02:39:01 +0000

    initial version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rulebasedmodels@304 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/Rule-Based_Models/Rule-Based_Models_files/Rule-Based_Models.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120518
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Rule-Based_Models_files/Rule-Based_ModelsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
