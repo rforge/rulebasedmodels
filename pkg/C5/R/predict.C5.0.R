996f91e1a9ca93c266c7e0601444a7cc7d0a7009
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-24 21:05:43 +0000

    added error trapping and support for trials
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rulebasedmodels@308 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/C5/R/predict.C5.0.R
+++ pkg/C5/R/predict.C5.0.R
@@ -1,21 +1,26 @@
 
-predict.C5.0 <- function (object, newdata = NULL, trials = 0, type = "class", ...) 
+predict.C5.0 <- function (object, newdata = NULL, trials = object$trials, type = "class", ...) 
 {
   if(!(type %in% c("class", "prob"))) stop("type should be either 'class' or 'prob'")
   if(is.null(newdata)) stop("newdata must be non-null")
   
-  ## check order of data to make sure that it is the same
-  ## XXX Not implemented yet
-  # newdata <- newdata[, object$vars$all,drop = FALSE]
+  ## make sure that the order of data to make sure that it is the same
+  newdata <- newdata[, object$predictors, drop = FALSE]
 
+  if(length(trials) > 1) stop("only one value of trials is allowed")
+  if(trials > object$trials) stop(paste("'trials' must be <=", object$trials, "for this object"))
 
   ## make cases file
   caseString <- makeDataFile(x = newdata, y = NULL)
-  print("whee")
-  print(newdata)
   
   ## for testing
   ##cat(caseString, '\n')
+
+  ## When passing trials to the C code, convert to
+  ## zero if the original version of trails is used
+
+  if(trials <= 0) stop("'trials should be a positive integer")
+  if(trials == object$trials) trials <- 0
 
   ## Add trials (not object$trials) as an argument
   Z <- .C("predictions",
@@ -23,14 +28,11 @@ predict.C5.0 <- function (object, newdata = NULL, trials = 0, type = "class", ..
           as.character(object$names),
           as.character(object$tree),
           as.character(object$rules),
-          predv = integer(nrow(newdata)),
-	  trials = integer(trials),
+          pred = integer(nrow(newdata)),
+          trials = as.integer(trials),
           output = character(1),
           PACKAGE = "C50"
           )
-
-  ## for testing
-  ##cat(Z$output, '\n')
 
   if(type == "class")
     {
