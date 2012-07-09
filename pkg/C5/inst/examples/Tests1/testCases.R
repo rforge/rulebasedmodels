776e9678f8d150c54fb0b436affadc6f1bb972e6
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-09 19:30:32 +0000

    added testing code for confidence values
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rulebasedmodels@330 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/C5/inst/examples/Tests1/testCases.R
+++ pkg/C5/inst/examples/Tests1/testCases.R
@@ -6,7 +6,7 @@ makeOptions <- function(x, base = ".")
     opt <- ifelse(x$cf != .25, paste(opt, "-c", x$cf*100), opt)
     opt <- ifelse(x$winnow, paste(opt, "-w"), opt)
     opt <- ifelse(x$subset, paste(opt, "-s"), opt)
-    opt <- ifelse(x$rules,  paste(opt, "-r"), opt)
+    opt <- ifelse(x$rules,  paste(opt, "-r"), opt)    
     opt <- ifelse(x$fuzzy,  paste(opt, "-p"), opt)
     opt <- ifelse(x$noGlobal, paste(opt, "-g"), opt)
     opt <- ifelse(x$trials > 1, paste(opt, "-b -t", x$trials), opt)
@@ -17,10 +17,10 @@ makeOptions <- function(x, base = ".")
   }
 
 makePredictOptions <- function(x , c50predict) {
-    opt <- "-f churnTestCase"
-    opt <- ifelse(x$rules,  paste(opt, "-r"), opt)
-    call <-paste(c50predict , opt)
-    call
+  opt <- "-f churnTestCase"
+  opt <- ifelse(x$rules,  paste(opt, "-r"), opt)
+  call <-paste(c50predict , opt)
+  call
 }
 
 
@@ -28,7 +28,7 @@ makeControl <- function(x)
   {
     C5.0Control(seed = 1, 
                 winnow = x$winnow, subset = x$subset,
-                rules = x$rules, fuzzyThreshold = x$fuzzy,
+                fuzzyThreshold = x$fuzzy,
                 noGlobalPruning = x$noGlobal,
                 sample = x$sample,
                 minCases = x$minCases,
@@ -61,14 +61,33 @@ makeHeader <- function(x, i = "")
         "\n", sep = "")
   }
 
+parsePredictions <- function(x, lvl = NULL)
+  {
+    startIndex <- grep("   1\t\t", x, fixed = TRUE)
+    x <- x[-(1:(startIndex - 1))]
+    lineStart <- regexpr("\t\t", x[length(x)])
+    x <- substring(x, lineStart+2)
+    x <- gsub("(\\[)|(\\])", "", x)
+    x <- strsplit(x, "[[:space:]]")
+    x <- lapply(x, function(x) x [x!= ""])
+    data.frame(obs = factor(unlist(lapply(x, function(x) x[1])), levels = lvl),
+               pred = factor(unlist(lapply(x, function(x) x[2])), levels = lvl),
+               conf = as.numeric(unlist(lapply(x, function(x) x[3]))))
+  }
+
 
 library(C50)
 data(churn)
-setwd(system.file("examples", "Tests1", package = "C50"))
+
 ## path to command line version
-c50Path <- "/Users/yorick/C50_original"
-#c50Path <- "~/Code/C50clean"
+c50Path    <- "/Users/yorick/C50_original"
 c50Predict <- "/Users/yorick/C50_original/tmp1/sample"
+
+c50Path    <- "/Users/kuhna03/Code/C50clean"
+c50Predict <- "/Users/kuhna03/Code/C5predict/sample"
+testPath   <- "/Users/kuhna03/Code/rulebasedmodels/pkg/C5/inst/examples/Tests1"
+
+setwd(testPath)
 
 combos <- expand.grid(bands = c(0, 3),
                       cf = c(.25, .75),
@@ -80,7 +99,7 @@ combos <- expand.grid(bands = c(0, 3),
                       trials = c(1, 12),
                       minCases = c(2, 10),
                       sample = c(0, .50))
-                      
+
 throwOut <- combos$bands & !combos$rules
 combos <- combos[!throwOut,]
 
@@ -90,10 +109,12 @@ outputs <- vector(mode = "list", length = nrow(combos))
 for(i in 1:nrow(combos))
   {
     makeHeader(combos[i,], i)
+    cat(rep("-", 60), "\n", sep = "")
     cat("   ", makeOptions(combos[i,], c50Path), "\n")
     expected <- system(makeOptions(combos[i,], c50Path), intern = TRUE)
     fit <- C5.0(churnTrain[,-20], churnTrain$churn,
                 trials = combos[i,"trials"],
+                rules = combos[i,"rules"],
                 control = makeControl(combos[i,]))
     outputs[[i]] <- list(expected = expected, observed = strsplit(fit$output, "\n")[[1]])
 
@@ -126,48 +147,31 @@ for(i in 1:nrow(combos))
     if(is.logical(results) && results) cat("passed!")
     cat("\n\n")
 
-    cat("\n\n")
-
     cat("   ", makePredictOptions(combos[i,], c50Predict), "\n")
 
     expected <- system(makePredictOptions(combos[i,], c50Predict), intern = TRUE)
-    print(churnTrain$churn)
-    fit <- predict.C5.0(churnTrain[,-20], churnTrain$churn,
-                trials = combos[i,"trials"],
-                control = makeControl(combos[i,]))
+    expected <- parsePredictions(expected, lvl = c("yes", "no"))
+    expConf <- expected$conf
 
-    #obs <- trimOutput(fit$output)
-    #expected <- trimOutput(expected, split = FALSE)
-    #
-    #results <- all.equal(obs, expected)
+    pred <- predict.C5.0(fit, churnTrain[,-20])
 
-    #if(!is.logical(results) || !results)
-    #  {
-    #    if(length(expected) == length(obs) && grepl("string mismatches", results))
-    #      {
-    #        cat("Differences:\n")
-    #        tmp <- cbind(expected[expected != obs],
-    #                     obs[expected != obs])
-    #        colnames(tmp) <- c("expected", "observed")
-    #        print(tmp)
-    #        cat("\n")
+    cat("   Classes:")
+    print(all.equal(expected$pred, pred))
+    predConf <- predict.C5.0(fit, churnTrain[,-20], type = "prob")
+    totalConf <- apply(predConf, 1, sum)
+    maxConf <- apply(predConf, 1, max)
+    names(maxConf) <- NULL
+    cat("   Probabilities:")
+    print(all.equal(round(maxConf, 4), round(expConf, 4)))
+    if(any(totalConf < .99) | (any(totalConf > 1.01)))
+      {
+        cat(" *do not add to one*\n")
+        print(summary(totalConf))
+      }
+    if(any(totalConf== 0)) cat("some have all probs == 0\n")    
+    cat("\n")
 
-    #      } else {
-    #        cat("failed! - output different lengths")
-    #        cat("\nExpected:\n")
-    #        print(expected)
-    #        cat("\nObserved:\n")
-    #        print(obs)
-    #        cat("\n")
-    #      }
-
-    #  }
-    #if(is.logical(results) && results) cat("passed!")
-    #cat("\n\n")
-
-    #cat("\n\n")
-
-    #rm(obs, expected, fit, results)
-
+    rm(fit, expected, expConf, pred, predConf, maxConf, totalConf)
+    unlink(c("churnTestCase.rules", "churnTestCase.tree", "churnTestCase.tmp"))
   }
 
